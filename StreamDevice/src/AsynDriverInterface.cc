01b442925e8d554b980c5bb58ce162dddcfa479f
Peter Denison <Peter.Denison@diamond.ac.uk>
2007-08-22 09:03:21 +0000

    streamDevice: Prevent I/O Intr processing until iocInit has completed

--- StreamDevice/src/AsynDriverInterface.cc
+++ StreamDevice/src/AsynDriverInterface.cc
@@ -926,6 +926,9 @@ void intrCallbackOctet(void* /*pvt*/, asynUser *pasynUser,
     AsynDriverInterface* interface =
         static_cast<AsynDriverInterface*>(pasynUser->userPvt);
 
+    // If we are still initialising during iocInit, ignore the input
+    if (INIT_RUN) return;
+
 // Problems here:
 // 1. We get this message too when we are the poller.
 //    Thus we have to ignore what we got from polling.
